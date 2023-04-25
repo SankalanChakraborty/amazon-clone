30e253be25b058b11e786f24a4a5d0948d136e3e
Sankalan Chakraborty <sankalanchakraborty.5@gmail.com>
2023-04-25 13:38:15 +0530

    working on product details page

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { products } from '../Utils/productData';
+import './ProductDetails.css';
 
 const ProductDetails = () => {
+    const {id} = useParams();
+    const getProduct = (product)=>{
+        return product.id === id
+    }
+    const product = products.find(getProduct);
+    console.log(product);
   return (
-    <div>
-      This is the product detail page
+    <div className="product-details__container">
+      <div className="product-image__container">
+        <img src={product.thumbnail} alt={product.title} />
+      </div>
+      <div className="product-information">
+        <h1>{product.title.longTitle}</h1>
+        <div className="price-and-discount__details">
+            <div>
+                <sup>₹</sup><span className="product-price">{Number(product.price.cost).toLocaleString("en-IN")}</span><span className="product-discount">-{product.price.discount}</span>
+            </div>
+            <div className="product-mrp">
+                <label>M.R.P.:</label> <span>₹{Number(product.price.mrp).toLocaleString("en-IN")}</span>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
