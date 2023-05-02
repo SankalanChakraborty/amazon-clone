d89c2095d756910030acd3c0ec232799712a51a8
Sankalan Chakraborty <sankalanchakraborty.5@gmail.com>
2023-05-02 22:10:59 +0530

    added subtotal logic

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Cart.css";
 import { useContext } from 'react';
 import { ProductContext } from '../Utils/context';
+import amazonOriginal from '../../Assets/Images/amazon-original.png'
+import { useEffect } from 'react';
 
 const Cart = () => {
+    const [subTotal, setSubTotal] = useState(0);
     const {cartItems} = useContext(ProductContext);
+
+    useEffect(()=>{
+      setSubTotal(cartItems.reduce((acc, currentItem)=> acc+Number(currentItem.price.cost), 0))
+    }, [cartItems])
+
   return (
     <>
     <div className="heading">
       <h1>Shopping Cart</h1>
     </div>
       <div className="cart__container">
+        <div className="cart-items">
       {cartItems.map(cartItem=>{
           return (
               <div className="item-details__container">
@@ -25,9 +34,21 @@ const Cart = () => {
                       </div>
                   </div>
               </div>
-          )
-      })}
+              )
+            })}
+        </div>
         <div className="checkout-section">
+            <div className="amazon-original-image__container">
+              <img src={amazonOriginal} alt="Amazon Original Logo" />
+            </div>
+            <div className="delivery-options">
+              <span>Your order is eligible for FREE Delivery</span>
+              <span>Select this option at checkout.</span>
+              <div className="subtotal">
+                <h3>Subtotal (1 items ): ₹ {subTotal}.00</h3>
+                <button className="btn-checkout">Proceed to Buy</button>
+              </div>
+            </div>
         </div>
       </div>
     </>
