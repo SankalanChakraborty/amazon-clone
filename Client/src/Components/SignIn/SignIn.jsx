c4d3e62ace698d09420a7630868f8cbbecf5e2b4
Sankalan Chakraborty <sankalanchakraborty.5@gmail.com>
2023-05-08 11:47:26 +0530

    added start and build script

--- Client/src/Components/SignIn/SignIn.jsx
+++ Client/src/Components/SignIn/SignIn.jsx
@@ -1,60 +1,74 @@
-import React, { useState } from 'react';
-import './SignIn.css';
-import logoBlack from '../../Assets/Images/amazon-logo-black.png';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./SignIn.css";
+import logoBlack from "../../Assets/Images/amazon-logo-black.png";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-
   const [logData, setLogData] = useState({
-    email:"",
-    password: ""
+    email: "",
+    password: "",
   });
-  
 
-  const setLoginData = (e)=>{
-    const {name, value} = e.target;
-    setLogData (()=>{
+  const setLoginData = (e) => {
+    const { name, value } = e.target;
+    setLogData(() => {
       return {
         ...logData,
-        [name]:value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
 
-  const userLogin = async(e)=>{
+  const userLogin = async (e) => {
     e.preventDefault();
     const response = await fetch("http://localhost:8000/signin", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(logData)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(logData),
     });
-    console.log(response);
-  }
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="sign-in__container">
-        <img src={logoBlack} alt="website-logo" />
-        <div className="sign-in__form-control">
-          <h1>Sign-In</h1>
-          <form method="POST" onSubmit={userLogin}>
-            <div className="email">
-              <label htmlFor="user-email">Email</label>
-              <input id="user-email" name="email" type="email" value={logData.email} onChange={setLoginData} required/>
-            </div>
-            <div className="password">
-              <label htmlFor="user-password">Password</label>
-              <input id="user-password" name="password" type="password" value={logData.password} onChange={setLoginData} required/>
-            </div>
-              <button type="submit">Continue</button>
-          </form>
-        </div>
-        <div className="new-user__section">
-            <p>New to Amazon?</p>
-            <Link to="/signup"><button>Create Your Amazon Account</button></Link>
-        </div>
+      <img src={logoBlack} alt="website-logo" />
+      <div className="sign-in__form-control">
+        <h1>Sign-In</h1>
+        <form method="POST" onSubmit={userLogin}>
+          <div className="email">
+            <label htmlFor="user-email">Email</label>
+            <input
+              id="user-email"
+              name="email"
+              type="email"
+              value={logData.email}
+              onChange={setLoginData}
+              required
+            />
+          </div>
+          <div className="password">
+            <label htmlFor="user-password">Password</label>
+            <input
+              id="user-password"
+              name="password"
+              type="password"
+              value={logData.password}
+              onChange={setLoginData}
+              required
+            />
+          </div>
+          <button type="submit">Continue</button>
+        </form>
+      </div>
+      <div className="new-user__section">
+        <p>New to Amazon?</p>
+        <Link to="/signup">
+          <button>Create Your Amazon Account</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignIn;
--- Server/server.js
+++ Server/server.js
@@ -1,39 +1,54 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const pool = require('./db');
+const pool = require("./db");
 const bcrypt = require("bcrypt");
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
-
 const PORT = process.env.PORT || 8000;
 
 // User Login
-app.post("/signin", async (req, res)=>{
-    const { email, password } = req.body;
-    try{
-        const checkUser = await pool.query("SELECT * FROM USERS WHERE EMAIL = $1", [email]);
-        console.log(checkUser.rows);
-    }catch(err){
-        console.error(err);
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const checkUser = await pool.query("SELECT * FROM USERS WHERE EMAIL = $1", [
+      email,
+    ]);
+    console.log(checkUser.rows);
+
+    if (!checkUser.rows.length) {
+      return res.status(400).json("User is not registered with amazon");
     }
-})
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // Register User
-app.post("/signup", async (req, res)=>{
-    const {fullName, mobileNum, email, setPassword} = req.body;
-    try{   
-        let hashedPassword = await bcrypt.hash(setPassword, 10);
-       const addUser = await pool.query("INSERT INTO USERS (NAME, MOBILE_NUMBER, EMAIL, PASSWORD) VALUES ($1, $2, $3, $4)", [fullName, mobileNum, email, hashedPassword]);
-       res.json("User added to the database");
-    }catch(error){
-        console.error("This is the error: ",error);
+app.post("/signup", async (req, res) => {
+  const { fullName, mobileNum, email, setPassword } = req.body;
+  try {
+    const checkUser = await pool.query("SELECT * FROM USERS WHERE EMAIL = $1", [
+      email,
+    ]);
+    if (!checkUser.rows.length) {
+      let hashedPassword = await bcrypt.hash(setPassword, 10);
+      const addUser = await pool.query(
+        "INSERT INTO USERS (NAME, MOBILE_NUMBER, EMAIL, PASSWORD) VALUES ($1, $2, $3, $4)",
+        [fullName, mobileNum, email, hashedPassword]
+      );
+      return res.json("User is successfully registered");
+    } else {
+      return res.status(400).json("User with this email is already present");
     }
-})
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
